fd906b3325eb95d87ec73e695188deb3f99a8084
Yohann Streibel <yohann.streibel@myscript.com>
2014-10-16 14:05:51 +0200

    [DEV] Fix build script

--- src/stroker.js
+++ src/stroker.js
@@ -1,11 +1,12 @@
 /**
+ * The Stroker class that can use to store writing strokes and manage the undo/redo/clear system
  *
+ * @class stroker
  * @param scope
  */
 (function (scope) {
 
     /**
-     *
      * @constructor
      */
     function Stroker () {
@@ -16,29 +17,29 @@
     }
 
     /**
-     *
+     * @description Initialize a stroker
      * @type {Object}
      */
     Stroker.prototype = Object.create(Object.prototype);
 
     /**
-     *
+     * @description Is Wrinting a stoke
      */
     Stroker.prototype.isWriting = function () {
         return this.writing;
     };
 
     /**
-     *
+     * @description Get the last current Stroke write
      */
     Stroker.prototype.getCurrentStroke = function () {
         return this.currentStroke;
     };
 
     /**
-     *
-     * @param x
-     * @param y
+     * @description Start to write a stroke
+     * @param x Abcisse coordinate
+     * @param y ordinate coordinate
      */
     Stroker.prototype.startStrokeWriting = function (x, y) {
         this.currentStroke = new Stroke();
@@ -48,9 +49,9 @@
     };
 
     /**
-     *
-     * @param x
-     * @param y
+     * @description Continue to write a stroke
+     * @param x Abcisse coordinate
+     * @param y ordinate coordinate
      */
     Stroker.prototype.continueStrokeWriting = function (x, y) {
         if (this.writing) {
@@ -60,7 +61,7 @@
     };
 
     /**
-     *
+     * @description End of writing a stroke
      * @param event
      */
     Stroker.prototype.endStrokeWriting = function (event) {
@@ -70,7 +71,7 @@
     };
 
     /**
-     *
+     * @description Clear the strokes list
      */
     Stroker.prototype.clear = function () {
         this.writing = false;
@@ -80,21 +81,21 @@
     };
 
     /**
-     *
+     * @description Is The Strokes list is empty
      */
     Stroker.prototype.isEmpty = function () {
         return this.strokes.length === 0;
     };
 
     /**
-     *
+     * @description Is the Undo/Redo Stack empty
      */
     Stroker.prototype.isRedoEmpty = function () {
         return this.undoRedoStack.length === 0;
     };
 
     /**
-     *
+     * @description Make an undo
      */
     Stroker.prototype.undo = function () {
         this.undoRedoStack.push(this.strokes[this.strokes.length - 1]);
@@ -102,7 +103,7 @@
     };
 
     /**
-     *
+     * @description Make a redo
      */
     Stroker.prototype.redo = function () {
         this.strokes.push(this.undoRedoStack[this.undoRedoStack.length - 1]);
@@ -110,39 +111,39 @@
     };
 
     /**
-     *
+     * @description Get the strokes list
      */
     Stroker.prototype.getStrokes = function () {
         return this.strokes;
     };
 
     /**
-     *
+     * @description Get the Undo/Redo Stack
      */
     Stroker.prototype.getUndoRedoStack = function () {
         return this.undoRedoStack;
     };
 
     /**
-     *
+     * @description Clear the Undo/Redo Stack
      */
     Stroker.prototype.clearUndoRedoStack = function () {
         this.undoRedoStack = [];
     };
 
     /**
-     *
-     * @param currentNonRecoStrokes
-     * @param index
+     * @description Copy the strokes values from index on an other list of strokes
+     * @param strokes List of strokes
+     * @param index Position to start the copy
      */
-    Stroker.prototype.copy = function (currentNonRecoStrokes, index) {
+    Stroker.prototype.copy = function (strokes, index) {
         for (index; index < this.strokes.length; index++) {
-            currentNonRecoStrokes.push(this.strokes[index]);
+            strokes.push(this.strokes[index]);
         }
     };
 
     /**
-     *
+     * @description Initialize the Stroker
      * @type {Stroker}
      */
     scope.Stroker = Stroker;
