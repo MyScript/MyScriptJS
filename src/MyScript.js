92b97fa93f972b1218645b8dac18e374740c53cb
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2015-02-25 15:11:33 +0100

    [DOC] Add dependencies to Q and Crypto on generated doc

--- src/MyScript.js
+++ src/MyScript.js
@@ -1,5 +1,9 @@
 /**
- @module MyScript
+ * MyScript javascript library to facilitate the use of cloud handwriting recognition
+ *
+ * @module MyScript
+ * @requires Q
+ * @requires CryptoJS
  */
 /*global MyScript:true */
 MyScript = {};
