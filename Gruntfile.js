5fd170839a8f555a629fefb1de418dff57945a0c
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2015-02-27 11:25:12 +0100

    [DEV] tests folders refactoring, splitted unit/func tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,285 +2,302 @@
 /* jshint camelcase:false */
 module.exports = function (grunt) {
 
-	// Load grunt tasks automatically
-	require('load-grunt-tasks')(grunt);
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
 
-	// Time how long tasks take. Can help when optimizing build times
-	require('time-grunt')(grunt);
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
 
-	grunt.initConfig({
+    grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json'),
-		license: grunt.file.read('LICENSE'),
-		bowerrc: grunt.file.readJSON('.bowerrc'),
-		fileList: grunt.file.readJSON('build.json'),
+        pkg: grunt.file.readJSON('package.json'),
+        license: grunt.file.read('LICENSE'),
+        bowerrc: grunt.file.readJSON('.bowerrc'),
+        fileList: grunt.file.readJSON('build.json'),
 
-		// Project settings
-		project: {
-			// variables
-			src: 'src',
-			tmp: '.tmp',
-			test: 'test',
-			func: 'test/func',
-			dist: 'dist',
-			docs: 'docs',
-			samples: 'samples'
-		},
+        // Project settings
+        project: {
+            // variables
+            src: 'src',
+            tmp: '.tmp',
+            test: 'test',
+            unit: 'unit',
+            func: 'func',
+            dist: 'dist',
+            docs: 'docs',
+            samples: 'samples',
+            test_results: 'test_results'
+        },
 
-		karma: {
+        karma: {
             options: {
-                configFile: '<%= project.test %>/karma.conf.js'
+                configFile: '<%= project.test %>/karma.conf.js',
+                singleRun: true,
+                background: false
             },
             unit: {
-                singleRun: true,
-                background: false,
-				options: {
-					files: [
-						'<%= bowerrc.directory %>/cryptojslib/components/core-min.js',
-						'<%= bowerrc.directory %>/cryptojslib/components/x64-core-min.js',
-						'<%= bowerrc.directory %>/cryptojslib/components/sha512-min.js',
-						'<%= bowerrc.directory %>/cryptojslib/components/hmac-min.js',
-						'<%= bowerrc.directory %>/q/q.js',
-						'<%= fileList %>',
-						'<%= project.test %>/spec/**/*.js'
-					]
-				}
-			},
-			func: {
-				singleRun: true,
-                background: false,
-				options: {
-					files: [
-						'<%= bowerrc.directory %>/cryptojslib/components/core-min.js',
-						'<%= bowerrc.directory %>/cryptojslib/components/x64-core-min.js',
-						'<%= bowerrc.directory %>/cryptojslib/components/sha512-min.js',
-						'<%= bowerrc.directory %>/cryptojslib/components/hmac-min.js',
-						'<%= bowerrc.directory %>/q/q.js',
-						'<%= project.dist %>/<%= pkg.name %>.min.js',
-						'<%= project.func %>/**/*.js'
-					]
-				}
+                options: {
+                    coverageReporter: {
+                        type: 'html',
+                        dir: '<%= project.test_results %>/<%= project.unit %>/coverage/'
+                    },
+                    files: [
+                        '<%= bowerrc.directory %>/cryptojslib/components/core-min.js',
+                        '<%= bowerrc.directory %>/cryptojslib/components/x64-core-min.js',
+                        '<%= bowerrc.directory %>/cryptojslib/components/sha512-min.js',
+                        '<%= bowerrc.directory %>/cryptojslib/components/hmac-min.js',
+                        '<%= bowerrc.directory %>/q/q.js',
+                        '<%= fileList %>',
+                        '<%= project.test %>/<%= project.unit %>/**/*.js'
+                    ]
+                }
+            },
+            func: {
+                options: {
+                    coverageReporter: {
+                        type: 'html',
+                        dir: '<%= project.test_results %>/<%= project.func %>/coverage/'
+                    },
+                    files: [
+                        '<%= bowerrc.directory %>/cryptojslib/components/core-min.js',
+                        '<%= bowerrc.directory %>/cryptojslib/components/x64-core-min.js',
+                        '<%= bowerrc.directory %>/cryptojslib/components/sha512-min.js',
+                        '<%= bowerrc.directory %>/cryptojslib/components/hmac-min.js',
+                        '<%= bowerrc.directory %>/q/q.js',
+                        '<%= project.dist %>/<%= pkg.name %>.min.js',
+                        '<%= project.test %>/<%= project.func %>/**/*.js'
+                    ]
+                }
             }
-		},
-		jshint: {
-			options: {
-				reporter: require('jshint-stylish-ex')
-			},
-			src: {
-				options: {
-					jshintrc: '.jshintrc'
-				},
-				src: [
-					'<%= project.src %>/**/*.js'
-				]
-			},
-			test: {
-				options: {
-					jshintrc: '<%= project.test %>/.mocha.jshintrc'
-				},
-				src: [
-					'<%= project.test %>/spec/**/*.js',
-					'<%= project.func %>/**/*.js'
-				]
-			}
-		},
-		concat: {
-			default: {
-				options: {
-					sourceMap: true
-				},
-				src: '<%= fileList %>',
-				dest: '<%= project.tmp %>/<%= pkg.name %>.js'
-			},
-			raw: {
-				src: '<%= fileList %>',
-				dest: '<%= project.dist %>/<%= pkg.name %>.js'
-			}
-		},
-		uglify: {
-			options: {
-				preserveComments: false,
-				mangle: false,
-				nonull: true,
-				sourceMap: true,
-				sourceMapIn: '<%= project.tmp %>/<%= pkg.name %>.js.map',
-				banner: '/*\n <%= pkg.name %> - <%= pkg.description %>\n Version: <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n License: <%= pkg.license %>\n */'
-			},
-			default: {
-				files: [{
-					expand: true,
-					flatten: true,
-					cwd: '<%= project.tmp %>',
-					src: '<%= pkg.name %>.js',
-					dest: '<%= project.dist %>',
-					ext: '.min.js'
-				}]
-			}
-		},
-		clean: {
-			tmp: {
-				files: [
-					{
-						dot: true,
-						src: [
-							'<%= project.tmp %>'
-						]
-					}
-				]
-			},
-			dist: {
-				files: [
-					{
-						dot: true,
-						src: [
-							'<%= project.dist %>'
-						]
-					}
-				]
-			},
-			test: {
-				files: [
-					{
-						dot: true,
-						src: [
-							'<%= project.test %>/results'
-						]
-					}
-				]
-			},
-			func: {
-				files: [
-					{
-						dot: true,
-						src: [
-							'<%= project.func %>/results'
-						]
-					}
-				]
-			}
-		},
-		copy: {
-			template: {
-				files: [{
-					expand: true,
-					dot: true,
-					cwd: '<%= bowerrc.directory %>/yuidoc-bootstrap3-theme/dist',
-					dest: '<%= project.tmp %>',
-					src: ['**']
-				}]
-			},
-			styles: {
-				files: [{
-					expand: true,
-					dot: true,
-					cwd: '<%= project.docs %>/styles',
-					dest: '<%= project.tmp %>/assets',
-					src: ['**']
-				}]
-			},
-			conf: {
-				files: [{
-					expand: true,
-					dot: true,
-					cwd: '<%= project.docs %>',
-					dest: '<%= project.tmp %>',
-					src: ['theme.json']
-				}]
-			},
-			samples: {
-				files: [{
-					expand: true,
-					dot: true,
-					cwd: '<%= project.samples %>',
-					dest: '<%= project.dist %>/<%= project.samples %>',
-					src: ['**']
-				}, {
-					expand: true,
-					dot: true,
-					flatten: true,
-					cwd: '<%= bowerrc.directory %>',
-					dest: '<%= project.dist %>/<%= project.samples %>/lib',
-					src: [
-						'cryptojslib/components/core-min.js',
-						'cryptojslib/components/x64-core-min.js',
-						'cryptojslib/components/sha512-min.js',
-						'cryptojslib/components/hmac-min.js',
-						'q/q.js'
-					]
-				}]
-			},
-			readme: {
-				files: [{
-					expand: true,
-					dot: true,
-					cwd: '',
-					dest: '<%= project.dist %>',
-					src: ['README.md']
-				}]
-			}
-		},
-		yuidoc: {
-			compile: {
-				name: '<%= pkg.name %>',
-				description: '<%= pkg.description %>',
-				version: '<%= pkg.version %>',
-				url: '<%= pkg.homepage %>',
-				options: {
-					exclude: 'third_party',
-					extension: '.js,.html',
-					paths: '<%= project.src %>',
-					outdir: '<%= project.dist %>/<%= project.docs %>',
-					linkNatives: 'true',
-					tabtospace: 2,
-					themedir: '<%= project.tmp %>',
-					helpers: ['<%= project.docs %>/helpers.js']
-				}
-			}
-		},
-		// Run some tasks in parallel to speed up the build process
-		concurrent: {
-			default: [
-				'test',
-				'docs'
-			]
-		}
-	});
+        },
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish-ex')
+            },
+            src: {
+                options: {
+                    jshintrc: '.jshintrc'
+                },
+                src: [
+                    '<%= project.src %>/**/*.js'
+                ]
+            },
+            test: {
+                options: {
+                    jshintrc: '<%= project.test %>/.mocha.jshintrc'
+                },
+                src: [
+                    '<%= project.test %>/<%= project.unit %>/**/*.js',
+                    '<%= project.test %>/<%= project.func %>/**/*.js'
+                ]
+            }
+        },
+        concat: {
+            default: {
+                options: {
+                    sourceMap: true
+                },
+                src: '<%= fileList %>',
+                dest: '<%= project.tmp %>/<%= pkg.name %>.js'
+            },
+            raw: {
+                src: '<%= fileList %>',
+                dest: '<%= project.dist %>/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                preserveComments: false,
+                mangle: false,
+                nonull: true,
+                sourceMap: true,
+                sourceMapIn: '<%= project.tmp %>/<%= pkg.name %>.js.map',
+                banner: '/*\n <%= pkg.name %> - <%= pkg.description %>\n Version: <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n License: <%= pkg.license %>\n */'
+            },
+            default: {
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    cwd: '<%= project.tmp %>',
+                    src: '<%= pkg.name %>.js',
+                    dest: '<%= project.dist %>',
+                    ext: '.min.js'
+                }]
+            }
+        },
+        clean: {
+            default: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%= project.tmp %>',
+                        '<%= project.dist %>',
+                        '<%= project.test_results %>'
+                    ]
+                }]
 
-	grunt.registerTask('default', [
-		'clean',
-		'concurrent',
-		'build',
-		'copy:readme'
-	]);
+            },
+            tmp: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%= project.tmp %>'
+                    ]
+                }]
+            },
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%= project.dist %>'
+                    ]
+                }]
+            },
+            test_unit_results: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%= project.test_results %>/<%= project.unit %>'
+                    ]
+                }]
+            },
+            test_func_results: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%= project.test_results %>/<%= project.func %>'
+                    ]
+                }]
+            }
+        },
+        copy: {
+            template: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= bowerrc.directory %>/yuidoc-bootstrap3-theme/dist',
+                    dest: '<%= project.tmp %>',
+                    src: ['**']
+                }]
+            },
+            styles: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= project.docs %>/styles',
+                    dest: '<%= project.tmp %>/assets',
+                    src: ['**']
+                }]
+            },
+            conf: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= project.docs %>',
+                    dest: '<%= project.tmp %>',
+                    src: ['theme.json']
+                }]
+            },
+            samples: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= project.samples %>',
+                    dest: '<%= project.dist %>/<%= project.samples %>',
+                    src: ['**']
+                }, {
+                    expand: true,
+                    dot: true,
+                    flatten: true,
+                    cwd: '<%= bowerrc.directory %>',
+                    dest: '<%= project.dist %>/<%= project.samples %>/lib',
+                    src: [
+                        'cryptojslib/components/core-min.js',
+                        'cryptojslib/components/x64-core-min.js',
+                        'cryptojslib/components/sha512-min.js',
+                        'cryptojslib/components/hmac-min.js',
+                        'q/q.js'
+                    ]
+                }]
+            },
+            readme: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '',
+                    dest: '<%= project.dist %>',
+                    src: ['README.md']
+                }]
+            }
+        },
+        yuidoc: {
+            compile: {
+                name: '<%= pkg.name %>',
+                description: '<%= pkg.description %>',
+                version: '<%= pkg.version %>',
+                url: '<%= pkg.homepage %>',
+                options: {
+                    exclude: 'third_party',
+                    extension: '.js,.html',
+                    paths: '<%= project.src %>',
+                    outdir: '<%= project.dist %>/<%= project.docs %>',
+                    linkNatives: 'true',
+                    tabtospace: 2,
+                    themedir: '<%= project.tmp %>',
+                    helpers: ['<%= project.docs %>/helpers.js']
+                }
+            }
+        },
+        // Run some tasks in parallel to speed up the build process
+        concurrent: {
+            default: [
+                'build',
+                'docs'
+            ]
+        }
+    });
 
-	grunt.registerTask('test', [
-		'clean:test',
-		'jshint',
-		'karma:unit'
-	]);
+    grunt.registerTask('default', [
+        'clean',
+        'jshint',
+        'test-unit',
+        'concurrent',
+        'copy:readme'
+    ]);
 
-	grunt.registerTask('build', [
-		'clean:tmp',
-		'concat',
-		'uglify',
-		'concat:raw',
-		'clean:tmp'
-	]);
+    grunt.registerTask('test', [
+        'test-unit',
+        'test-func'
+    ]);
 
-	grunt.registerTask('func', [
-		'clean:func',
-		'jshint',
-		'karma:func'
-	]);
+    grunt.registerTask('test-unit', [
+        'clean:test_unit_results',
+        'jshint:test',
+        'karma:unit'
+    ]);
 
-	grunt.registerTask('docs', [
-		'clean:tmp',
-		'copy:template',
-		'copy:styles',
-		'copy:conf',
-		'yuidoc',
-		'copy:samples',
-		'clean:tmp'
-	]);
+    grunt.registerTask('test-func', [
+        'clean:test_func_results',
+        'jshint:test',
+        'karma:func'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean:tmp',
+        'concat',
+        'uglify',
+        'concat:raw',
+        'clean:tmp'
+    ]);
+
+    grunt.registerTask('docs', [
+        'clean:tmp',
+        'copy:template',
+        'copy:styles',
+        'copy:conf',
+        'yuidoc',
+        'copy:samples',
+        'clean:tmp'
+    ]);
 
 };
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,10 +27,6 @@ module.exports = function (config) {
 		reporters: ['progress', 'mocha', 'coverage'],
 
 		// optionally, configure the reporter
-		coverageReporter: {
-			type: 'html',
-			dir: 'test/results/coverage/'
-		},
 
 		// web server port
 		port: 9876,
@@ -40,7 +36,7 @@ module.exports = function (config) {
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_DEBUG,
+		logLevel: config.LOG_INFO,
 
 		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch: false,
