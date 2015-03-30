6ecc3f9a343f644d12cbe81f4781a4e89223c463
Olivier Berot <olivier.berot@myscript.com>
2015-03-30 15:34:20 +0200

    [func tests] suppress unused input files

--- test/func/in/inkfiles/math/result/fractionS1.ml
+++
@@ -1,9 +0,0 @@
-<math xmlns="http://www.w3.org/1998/Math/MathML">
-  <mstyle displaystyle="true">
-    <mn> 8 </mn>
-    <mi> R </mi>
-    <mo> + </mo>
-    <mn> 7 </mn>
-    <mi> s </mi>
-  </mstyle>
-</math>
--- test/func/in/inkfiles/math/result/lettra1.ml
+++
@@ -1,9 +0,0 @@
-<math xmlns="http://www.w3.org/1998/Math/MathML">
-  <mstyle displaystyle="true">
-    <mo> + </mo>
-    <mi> t </mi>
-    <mo> + </mo>
-    <mi> t </mi>
-    <mi> T </mi>
-  </mstyle>
-</math>
