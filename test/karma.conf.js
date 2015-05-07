33283134059fe62a018d7045c1d13c348f0467bd
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2015-05-07 15:08:00 +0200

    [FIX] Add karma spec reporter

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress', 'mocha', 'coverage', 'html', 'junit'],
+        reporters: ['spec', 'mocha', 'coverage', 'html', 'junit'],
 
 		// optionally, configure the reporter
 
