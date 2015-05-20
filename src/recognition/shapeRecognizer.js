ebaeab7f7d58121dd3505c1557e69132dab8d2da
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2015-05-20 18:08:51 +0200

    [FIX] rollback protocole status since certificate was fixed

--- src/recognition/shapeRecognizer.js
+++ src/recognition/shapeRecognizer.js
@@ -75,7 +75,7 @@
             data.setHmac(this.computeHmac(applicationKey, input, hmacKey));
         }
 
-        return this.http.post('//' + this.host + '/api/v3.0/recognition/rest/shape/doSimpleRecognition.json', data).then(
+        return this.http.post('http://' + this.host + '/api/v3.0/recognition/rest/shape/doSimpleRecognition.json', data).then(
             function success(response) {
                 return new scope.ShapeResult(response);
             },
@@ -99,7 +99,7 @@
             instanceSessionId: instanceId
         };
 
-        return this.http.post('//' + this.host + '/api/v3.0/recognition/rest/shape/clearSessionId.json', data).then(
+        return this.http.post('http://' + this.host + '/api/v3.0/recognition/rest/shape/clearSessionId.json', data).then(
             function success(response) {
                 return response;
             },
