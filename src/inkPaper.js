41ed9c66d1ec0b130f6d2535946fa2692e137631
Pierre-Alban DEWITTE <pad@myscript.com>
2016-04-08 18:04:07 +0200

    [DEV] Adding a getStats method on every components to be able to monitor your cartridge usage

--- src/inkPaper.js
+++ src/inkPaper.js
@@ -1125,6 +1125,42 @@
     };
 
     /**
+     * Return the stats allowing to monitor what ink size is send to the server.
+     * @returns Stats objects format {strokesCount : 0, pointsCount : 0, byteSize : 0, humanSize : 0, humanUnit : 'BYTE'} humanUnit could have the values BYTE, BYTES, KiB, MiB
+     */
+    InkPaper.prototype.getStats = function () {
+        var stats = {strokesCount : 0, pointsCount : 0, byteSize : 0, humanSize : 0, humanUnit : 'BYTE'};
+        if(this.components){
+            stats.strokesCount = this.components.length;
+            var pointsCount = 0;
+            for(var strokeNb = 0; strokeNb < this.components.length; strokeNb++){
+                pointsCount = pointsCount + this.components[strokeNb].x.length;
+            }
+            stats.strokesCount = this.components.length;
+            stats.pointsCount = pointsCount;
+            //We start with 270 as it is the size in bytes. Make a real computation implies to recode a doRecogntion
+            var byteSize = 270;
+            byteSize = JSON.stringify(this.components).length;
+            stats.byteSize = byteSize;
+            if (byteSize < 270) {
+                stats.humanUnit = 'BYTE';
+                stats.byteSize = 0;
+                stats.humanSize  = 0;
+            } else if (byteSize < 2048) {
+                stats.humanUnit = 'BYTES';
+                stats.humanSize  = byteSize;
+            } else if (byteSize < 1024 * 1024) {
+                stats.humanUnit = 'KiB';
+                stats.humanSize = (byteSize / 1024).toFixed(2);
+            } else {
+                stats.humanUnit = 'MiB';
+                stats.humanSize = (byteSize / 1024 / 1024).toFixed(2);
+            }
+        }
+        return stats;
+    };
+
+    /**
      * Tool to create canvas
      *
      * @private
