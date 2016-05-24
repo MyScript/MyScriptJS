3ff104ca43f55180b8297dafed783a5a03a8b045
DEWITTE Pierre-Alban <pierre-alban.dewitte@myscript.com>
2016-05-24 17:31:25 +0200

    [DEV] Splitting nightwatch tests to have better readability

--- dist/myscript.min.js
+++ dist/myscript.min.js
@@ -1,6 +1,6 @@
 /**
  * myscript - The JavaScript library for the MyScript Cloud recognition service
- * @version v2.0.0-beta-3-gb6078c5
+ * @version v2.0.0-beta-4-g432db18
  * @link https://myscript.github.io/MyScriptJS/
  * @license Apache-2.0
  */
