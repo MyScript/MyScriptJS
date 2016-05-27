605e042c86f80de6a2820db8dd0039f90ff7003d
DEWITTE Pierre-Alban <pierre-alban.dewitte@myscript.com>
2016-05-27 16:06:20 +0200

    [DEV]Updating with last version

--- dist/myscript.min.css
+++ dist/myscript.min.css
@@ -1,6 +1,6 @@
 /**
  * myscript - The JavaScript library for the MyScript Cloud recognition service
- * @version 46bc66c
+ * @version f6c9736
  * @link https://myscript.github.io/MyScriptJS/
  * @license Apache-2.0
  */
