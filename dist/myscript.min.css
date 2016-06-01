2e93cdad5921b881be1caaf6a49f939df1c546e6
DEWITTE Pierre-Alban <pierre-alban.dewitte@myscript.com>
2016-06-01 18:03:39 +0200

    [BUILD]Improving makefile

--- dist/myscript.min.css
+++ dist/myscript.min.css
@@ -1,6 +1,6 @@
 /**
  * myscript - The JavaScript library for the MyScript Cloud recognition service
- * @version 605e042
+ * @version 365b4ac
  * @link https://myscript.github.io/MyScriptJS/
  * @license Apache-2.0
  */
