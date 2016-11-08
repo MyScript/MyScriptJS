b95b5612591a3b01da6c5c0729f958fe55ee7225
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2016-11-08 11:45:40 +0100

    [FIX] fixes obsolete tests

--- test/mocha/partial/model/InkModel.spec.babel.js
+++ test/mocha/partial/model/InkModel.spec.babel.js
@@ -6,14 +6,13 @@ import * as Point from '../../../../src/model/Point';
 
 describe('Testing InkModel', () => {
   it('Creating a model and update pending strokes', () => {
-    const POINTER_ID = 1;
     const initialModel = InkModel.createModel();
-    const updatedModel1 = InkModel.endPendingStroke(initialModel, Point.createPoint(1, 1), POINTER_ID);
-    const updatedModel2 = InkModel.appendToPendingStroke(updatedModel1, Point.createPoint(2, 2), POINTER_ID);
-    const updatedModel3 = InkModel.appendToPendingStroke(updatedModel2, Point.createPoint(3, 3), POINTER_ID);
-    const updatedModel4 = InkModel.initPendingStroke(updatedModel3, Point.createPoint(4, 4), POINTER_ID);
+    const updatedModel1 = InkModel.initPendingStroke(initialModel, Point.createPoint(1, 1));
+    const updatedModel2 = InkModel.appendToPendingStroke(updatedModel1, Point.createPoint(2, 2));
+    const updatedModel3 = InkModel.appendToPendingStroke(updatedModel2, Point.createPoint(3, 3));
+    const updatedModel4 = InkModel.endPendingStroke(updatedModel3, Point.createPoint(4, 4));
     logger.debug('Last model is ', updatedModel4);
-    assert.notDeepEqual(initialModel.pendingStrokes, updatedModel4.pendingStrokes, 'Model should not shared array references');
+    assert.deepEqual(initialModel, updatedModel4);
   });
 
 
--- test/mocha/partial/util/ModelStats.spec.babel.js
+++ test/mocha/partial/util/ModelStats.spec.babel.js
@@ -7,14 +7,13 @@ import * as ModelStats from '../../../../src/util/ModelStats';
 
 describe('Testing InkModel Stats', () => {
   it('Testing generated stats', () => {
-    const POINTER_ID = 1;
     const initialModel = InkModel.createModel();
-    const updatedModel1 = InkModel.endPendingStroke(initialModel, Point.createPoint(1, 1), POINTER_ID);
-    const updatedModel2 = InkModel.appendToPendingStroke(updatedModel1, Point.createPoint(2, 2), POINTER_ID);
-    const updatedModel3 = InkModel.appendToPendingStroke(updatedModel2, Point.createPoint(3, 3), POINTER_ID);
-    const updatedModel4 = InkModel.initPendingStroke(updatedModel3, Point.createPoint(4, 4), POINTER_ID);
+    const updatedModel1 = InkModel.initPendingStroke(initialModel, Point.createPoint(1, 1));
+    const updatedModel2 = InkModel.appendToPendingStroke(updatedModel1, Point.createPoint(2, 2));
+    const updatedModel3 = InkModel.appendToPendingStroke(updatedModel2, Point.createPoint(3, 3));
+    const updatedModel4 = InkModel.endPendingStroke(updatedModel3, Point.createPoint(4, 4));
     logger.debug('Last model stats are ', ModelStats.computeStats(updatedModel4));
-    assert.notDeepEqual(initialModel.pendingStrokes, updatedModel4.pendingStrokes, 'Model should not shared array references');
+    assert.deepEqual(initialModel, updatedModel4);
   });
 
 
