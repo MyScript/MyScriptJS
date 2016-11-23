750c66e5b38cc704b609558dc55c559af5e98732
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2016-11-23 09:27:19 +0100

    [FIX] fixes tests

--- test/mocha/partial/configuration/MyScriptJSBehaviors.spec.babel.js
+++ test/mocha/partial/configuration/MyScriptJSBehaviors.spec.babel.js
@@ -1,19 +1,20 @@
 import { describe, it } from 'mocha';
 import { assert } from 'chai';
 import * as MyScriptJSParameter from '../../../../src/configuration/MyScriptJSParameter';
+import * as MyScriptJSBehaviors from '../../../../src/configuration/MyScriptJSBehaviors';
 import { testLogger } from '../../../../src/configuration/LoggerConfig';
 
 describe('MyScriptJSParameter check', () => {
   it('Testing createDefaultBehavioursFromPaperOptions', () => {
     const defaultMyScriptJSParameter = MyScriptJSParameter.enrichParametersWithDefault();
-    const defaultBehaviors = MyScriptJSParameter.createDefaultBehavioursFromPaperOptions(defaultMyScriptJSParameter);
+    const defaultBehaviors = MyScriptJSBehaviors.createDefaultBehavioursFromPaperOptions(defaultMyScriptJSParameter);
     assert.strictEqual(defaultBehaviors.optimizedParameters.triggerRecognitionOn, 'PEN_UP', 'PEN_UP should be the default value for V3_WEBSOCKET_TEXT');
   });
 
   it('Testing createDefaultBehavioursFromPaperOptions - MATH - WS', () => {
     const defaultMyScriptJSParameter = MyScriptJSParameter.enrichParametersWithDefault();
     defaultMyScriptJSParameter.recognitionParams.type = 'MATH';
-    const defaultBehaviors = MyScriptJSParameter.createDefaultBehavioursFromPaperOptions(defaultMyScriptJSParameter);
+    const defaultBehaviors = MyScriptJSBehaviors.createDefaultBehavioursFromPaperOptions(defaultMyScriptJSParameter);
     assert.strictEqual(defaultBehaviors.optimizedParameters.triggerRecognitionOn, 'PEN_UP', 'PEN_UP should be the default value for V3_WEBSOCKET_MATH');
   });
 
@@ -21,7 +22,7 @@ describe('MyScriptJSParameter check', () => {
     const defaultMyScriptJSParameter = MyScriptJSParameter.enrichParametersWithDefault();
     defaultMyScriptJSParameter.recognitionParams.type = 'MATH';
     defaultMyScriptJSParameter.recognitionParams.protocol = 'REST';
-    const defaultBehaviors = MyScriptJSParameter.createDefaultBehavioursFromPaperOptions(defaultMyScriptJSParameter);
+    const defaultBehaviors = MyScriptJSBehaviors.createDefaultBehavioursFromPaperOptions(defaultMyScriptJSParameter);
     assert.strictEqual(defaultBehaviors.optimizedParameters.triggerRecognitionOn, 'QUIET_PERIOD', 'QUIET_PERIOD should be the default value for V3_REST_MATH');
   });
 });
