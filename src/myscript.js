c07865f26ee079ae56b0a348a2b47ef934b77b3f
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2016-11-28 16:21:14 +0100

    [DEV] Remove useless InkPaper v1 wrapper

--- src/myscript.js
+++ src/myscript.js
@@ -1,12 +1,10 @@
 import DebugConfig from './configuration/DebugConfig';
 import MyScriptJSConstants from './configuration/MyScriptJSConstants';
 import { InkPaper2, register, getAvailableLanguageList } from './InkPaper2';
-import { InkPaper } from './InkPaper';
 
 export {
     DebugConfig,
     MyScriptJSConstants,
-    InkPaper,
     InkPaper2,
     register,
     getAvailableLanguageList
