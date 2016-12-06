b972bf362406528be99a36e7cdd26690f29acaa5
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2016-12-06 10:35:19 +0100

    [DEV] Add test on default parameters assignment

---
+++ test/mocha/partial/configuration/MyScriptJSParameter.spec.babel.js
@@ -0,0 +1,20 @@
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+import * as MyScriptJSParameter from '../../../../src/configuration/MyScriptJSParameter';
+import * as MyScriptJSBehaviors from '../../../../src/configuration/MyScriptJSBehaviors';
+
+describe('Check parameters', () => {
+  const customConfiguration = {
+    recognitionParams: {
+      type: 'SHAPE',
+      protocol: 'REST'
+    }
+  };
+
+  it(`Should merge default configuration and ${JSON.stringify(customConfiguration)}`, () => {
+    const parameters = MyScriptJSParameter.enrichPaperParametersWithDefault(customConfiguration);
+    assert.strictEqual(parameters.recognitionParams.type, customConfiguration.recognitionParams.type, `${customConfiguration.recognitionParams.type} should be the default value for ${parameters} type`);
+    assert.strictEqual(parameters.recognitionParams.protocol, customConfiguration.recognitionParams.protocol, `${customConfiguration.recognitionParams.protocol} should be the default value for ${parameters} protocol`);
+    assert.isDefined(parameters.recognitionParams.server, 'recognitionParams.server should keep its default value');
+  });
+});
