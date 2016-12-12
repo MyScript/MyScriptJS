cdac9a74fd4f505d94a91532e62227508f522cb1
DEWITTE Pierre-Alban <pierre-alban.dewitte@myscript.com>
2016-12-12 18:15:04 +0100

    [Selenium tests] All recogntions are now OK

--- tools/convertInkToArrayOfXY.js
+++ tools/convertInkToArrayOfXY.js
@@ -2,13 +2,18 @@ const path = require('path');
 const fs = require('fs');
 const readline = require('readline');
 
-const file = '/home/padewitte/projets/NewMyScriptJS/target/music_basic_1.ink';
+const file = '/home/padewitte/projets/NewMyScriptJS/target/FourShapeText.ink';
 const lineReader = readline.createInterface({ input: fs.createReadStream(file) });
 
 const strokes = [];
 let x = [];
 let y = [];
 let nbPoints = 0;
+let minX = 1000;
+let maxX = -1000;
+let minY = 1000;
+let maxY = -1000;
+
 
 lineReader.on('line', (line) => {
   if (line.indexOf(' ') === -1) {
@@ -20,28 +25,23 @@ lineReader.on('line', (line) => {
     console.log('### => ' + line);
   } else {
     nbPoints++;
-    if ((nbPoints % 3) === 0) {
+    if ((nbPoints % 2) === 0) {
       const splittedCoord = line.split(' ');
-      x.push(Number.parseInt(((0 + (Number.parseInt(splittedCoord[0], 0))) / 1), 0));
-      y.push(Number.parseInt(((-252 + (Number.parseInt(splittedCoord[1], 0))) / 1), 0));
+      const newX = Number.parseInt(((0 + (Number.parseInt(splittedCoord[0], 0))) / 1), 0);
+      x.push(newX);
+      const newY = Number.parseInt(((-100 + (Number.parseInt(splittedCoord[1], 0))) / 1), 0);
+      y.push(newY);
+      if (newX > maxX) maxX = newX;
+      if (newX < minX) minX = newX;
+      if (newY > maxY) maxY = newY;
+      if (newY < minY) minY = newY;
       console.log('*** => ' + splittedCoord);
     }
   }
 });
 
 lineReader.on('close', () => {
-  strokes.shift();
-  strokes.shift();
-  strokes.shift();
-  strokes.shift();
-  strokes.shift();
-  strokes.shift();
-  strokes.shift();
-  strokes.shift();
-  strokes.shift();
-  strokes.shift();
-  strokes.shift();
-
+  console.log({ minX, maxX, minY, maxY });
   process.stdout.write(JSON.stringify(strokes));
 });
 
