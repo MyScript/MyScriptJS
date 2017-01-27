925d921db602bf3966ccc82977d663d39a033a9e
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2017-01-27 12:51:58 +0100

    [FIX] restore shape simple test

--- test/nightwatch/partial/05-shape-rest-cdkv3.js
+++ test/nightwatch/partial/05-shape-rest-cdkv3.js
@@ -3,6 +3,12 @@ const inkPlayer = require('../lib/inkPlayer');
 
 const shapeConfig = config.shapeRestSample;
 
+module.exports['Shape very simple test'] = function (browser) {
+  shapeConfig.inks.forEach((ink) => {
+    inkPlayer.playInk(browser, shapeConfig, ink.strokes, ink.labels, '#inkPaperSupervisor span', '#inkPaperSupervisor span');
+  });
+};
+
 module.exports['Shape multiple undos test'] = function (browser) {
   shapeConfig.inks.forEach((ink) => {
     inkPlayer.playInkMultipleUndos(browser, shapeConfig, ink.strokes, ink.labels, '#inkPaperSupervisor span', '#inkPaperSupervisor span');
