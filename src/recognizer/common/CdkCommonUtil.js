dd20f97596ef5d5cae4e916e88c7c6811b8d2791
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2017-03-03 15:47:29 +0100

    [DEV] fixes missing symboltree math recognition result in event

--- src/recognizer/common/CdkCommonUtil.js
+++ src/recognizer/common/CdkCommonUtil.js
@@ -10,10 +10,15 @@ export function extractRecognitionResult(model) {
   const result = model.rawResults.recognition.result;
   if (result && result.results) {
     return result.results
-        .filter(item => Object.keys(MyScriptJSConstants.MIME).includes(item.type))
         .map((item) => {
           const res = {};
-          res[MyScriptJSConstants.MIME[`${item.type}`]] = item.value;
+          if (Object.keys(MyScriptJSConstants.MIME).includes(item.type)) {
+            res[MyScriptJSConstants.MIME[`${item.type}`]] = item.value;
+          } else if (Object.keys(item).includes('root')) {
+            res[`${item.type}`] = item.root;
+          } else {
+            res[`${item.type}`] = item.value;
+          }
           return res;
         })
         .reduce((a, b) => Object.assign(a, b));
