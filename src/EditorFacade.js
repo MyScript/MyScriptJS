46f31dfa74382596ce20ce76516f04fcab5bbfb8
Romain Cascino <romain.cascino@myscript.com>
2017-10-09 11:05:25 +0200

    [FIX] Languages now retrieved from local json file

--- src/EditorFacade.js
+++ src/EditorFacade.js
@@ -1,8 +1,6 @@
+import * as languagesJson from './configuration/languages.json';
 import { editorLogger as logger } from './configuration/LoggerConfig';
 import { Editor } from './Editor';
-import * as DefaultConfiguration from './configuration/DefaultConfiguration';
-import Constants from './configuration/Constants';
-import * as NetworkInterface from './recognizer/rest/networkInterface';
 
 /**
  * Attach an Editor to a DOMElement
@@ -20,29 +18,8 @@ export function register(element, configuration, penStyle, theme, behaviors) {
 
 /**
  * Return the list of available recognition languages
- * @param {Configuration} configuration Current configuration
- * @param {Boolean} [sortByValue=true] True if the language list should be sort by value, false otherwise
- * @return {Promise.<Object>} A list of languages available for the current configuration
+ * @return {JSON} A list of available languages
  */
-export function getAvailableLanguageList(configuration, sortByValue = true) {
-  const innerConfiguration = DefaultConfiguration.overrideDefaultConfiguration(configuration);
-  const data = {
-    applicationKey: innerConfiguration.recognitionParams.server.applicationKey,
-    sortByValue
-  };
-
-  switch (innerConfiguration.recognitionParams.type) {
-    case Constants.RecognitionType.TEXT:
-      data.inputMode = innerConfiguration.recognitionParams.v3.textParameter.textInputMode;
-      break;
-    case Constants.RecognitionType.ANALYZER:
-      data.inputMode = innerConfiguration.recognitionParams.v3.analyzerParameter.textParameter.textInputMode;
-      break;
-    default:
-      break;
-  }
-
-  const suffixUrl = innerConfiguration.recognitionParams.apiVersion === 'V3' ? 'api/v3.0/recognition/rest/text/availableLanguageList.json' : 'api/v4.0/iink/availableLanguageList.json';
-
-  return NetworkInterface.get({}, `${innerConfiguration.recognitionParams.server.scheme}://${innerConfiguration.recognitionParams.server.host}/${suffixUrl}`, data);
+export function getAvailableLanguageList() {
+  return languagesJson;
 }
