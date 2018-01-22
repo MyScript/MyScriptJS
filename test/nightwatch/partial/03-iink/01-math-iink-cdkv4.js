6535838a6ee08301ad995042741b8c6301bd7dab
Olivier Berot <olivier.berot@myscript.com>
2018-01-22 12:05:43 +0100

    different ink for label and undo-redo tests

--- test/nightwatch/partial/03-iink/01-math-iink-cdkv4.js
+++ test/nightwatch/partial/03-iink/01-math-iink-cdkv4.js
@@ -1,20 +1,29 @@
 const inkPlayer = require('../../lib/inkPlayer');
 const config = require('../../../lib/configuration').getConfiguration('MATH', 'WEBSOCKET', 'V4');
 
-function runInkTests(ink) {
-  // module.exports[config.header + ' checkUndoRedo ' + ink.name] = function checkUndoRedo(browser) {
-  //   inkPlayer.checkUndoRedo(browser, config, ink.strokes, ink.exports.LATEX, '#editor', '[data-key="application/x-latex"]');
-  // };
-
+function runLabelTests(ink) {
   // module.exports[config.header + ' checkConvert ' + ink.name] = function checkConvert(browser) {
   //   inkPlayer.checkConvert(browser, config, ink.strokes, ink.exports.LATEX, '#editor', '[data-key="application/x-latex"]');
   // };
+  module.exports[config.header + ' checkLabels ' + ink.name] = function checkLabels(browser) {
+    inkPlayer.checkLabels(browser, config, ink.strokes, ink.exports.LATEX, '#editor', '[data-key="application/x-latex"]');
+  };
+}
 
+function runUndoTests(ink) {
+  // module.exports[config.header + ' checkUndoRedo ' + ink.name] = function checkUndoRedo(browser) {
+  //   inkPlayer.checkUndoRedo(browser, config, ink.strokes, ink.exports.LATEX, '#editor', '[data-key="application/x-latex]');
+  // };
   module.exports[config.header + ' checkUndoRedoReconnect ' + ink.name] = function checkUndoRedoReconnect(browser) {
     inkPlayer.checkUndoRedoReconnect(browser, config, ink.strokes, ink.exports.LATEX, '#editor', '[data-key="application/x-latex"]');
   };
 }
 
 config.inks
-  .filter(ink => ['equation'].includes(ink.name))
-  .forEach(ink => runInkTests(ink));
+  .filter(ink => ['equation2'].includes(ink.name))
+  .forEach(ink => runLabelTests(ink));
+
+config.inks
+  .filter(ink => ['equation3'].includes(ink.name))
+  .forEach(ink => runUndoTests(ink));
+
