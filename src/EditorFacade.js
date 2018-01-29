57eecd3c80ec8e0dc36bac55dbe208aba02f968d
Romain Cascino <romain.cascino@myscript.com>
2018-01-29 15:18:54 +0100

    [FIX] Get languages for V3

--- src/EditorFacade.js
+++ src/EditorFacade.js
@@ -1,6 +1,8 @@
 import * as languagesJson from './configuration/languages.json';
+import * as languagesJsonV3 from './configuration/languagesV3.json';
 import { editorLogger as logger } from './configuration/LoggerConfig';
 import { Editor } from './Editor';
+import * as DefaultConfiguration from './configuration/DefaultConfiguration';
 
 /**
  * Attach an Editor to a DOMElement
@@ -18,8 +20,10 @@ export function register(element, configuration, penStyle, theme, behaviors) {
 
 /**
  * Return the list of available recognition languages
+ * @param {Configuration} [configuration] Configuration to get the languages
  * @return {JSON} A list of available languages
  */
-export function getAvailableLanguageList() {
-  return languagesJson;
+export function getAvailableLanguageList(configuration) {
+  const innerConfiguration = DefaultConfiguration.overrideDefaultConfiguration(configuration);
+  return innerConfiguration.recognitionParams.apiVersion === 'V4' ? languagesJson : languagesJsonV3;
 }
